efea5445e86f1ed0fa2439ad202687553a865686
Kartik Jolapara <kjmickey003@gmail.com>
2023-04-09 07:01:54 +0530

    generate page
    Signed-off-by: Kartik Jolapara <kjmickey003@gmail.com>

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,25 @@
-import { Routes, Route } from 'react-router-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+import { Routes, Route } from 'react-router-dom';
 
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 import Dashboard from './pages/Dashboard';
 import Profile from './pages/Profile';
+import Generate from './pages/Generate';
 
 const App = () => {
   return (
     <div>
       <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/signup' element={<SignupPage />} />
-        <Route path='/dashboard' element={<Dashboard />} />
-        <Route path='/profile' element={<Profile />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/generate" element={<Generate />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </div>
   );
